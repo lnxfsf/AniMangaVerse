738577c84c4bbae3b4170446ed885ed717004827
DraganM03 <draganmijatovic.s1a@gmail.com>
2023-10-01 14:36:59 +0200

    Clickable animes in PopularC

--- src/components/PopularCarousel.jsx
+++ src/components/PopularCarousel.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { PopularItem } from "./PopularItem";
 import { anime_data } from "../data";
 import "../styles/PopularCarousel.css";
@@ -22,8 +23,13 @@ const PopularCarousel = () => {
 
   return (
     <>
+     <Link
+                  to="/detailspage"
+                  state={{ id: curAnime.anime_id, anime: true }}
+                  key={curAnime.anime_id}
+      >
       <PopularItem  title={curAnime.title} subtitle={curAnime.subtitle} image={curAnime.image} seasons={curAnime.seasons} year={curAnime.start_date} age_restriction={curAnime.age_restriction} about={curAnime.desc} genres={curAnime.genre} />
-    </>
+    </Link></>
   );
 }
 
