044dd5dfdfcc65c0df50380f6d47506bbdbd1d36
Linuxiness <igor.lerinc7@gmail.com>
2023-11-12 12:37:43 +0100

    convert graphql to api format

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,59 +13,148 @@ import requests
 from django.http import JsonResponse
 import json
 
+
+
+
+
+def format_anime_data(graphql_data):
+    media_data = graphql_data['data']['data']['Media']
+
+    # Transform genres list into a single string
+    genres_string = "; ".join(media_data['genres'])
+
+    # Create a dictionary in the desired format
+    formatted_data = {
+        "id": media_data['id'],
+        "title": media_data['title']['english'],
+        "genre": genres_string,
+        # Add other fields as needed
+    }
+
+    return formatted_data
+
 def anilist_graphql_view(request):
-
-    # GraphQL API endpoint URL
-    graphql_url = 'https://graphql.anilist.co'
-    
-    
-    # Define the GraphQL query
-    query = '''
-    query ($id: Int) {
-      Media (id: $id, type: ANIME) {
-        id
-        title {
-          romaji
-          english
-          native
+    # Assuming you have the GraphQL response data stored in a variable named graphql_data
+    graphql_data = {
+        "data": {
+            "data": {
+                "Media": {
+                    "id": 15125,
+                    "title": {"english": "Teekyuu"},
+                    "genres": ["Comedy", "Slice of Life", "Sports"],
+                    # Add other fields as needed
+                }
+            }
         }
-      }
-    }
-    '''
-    
-    
-    # Define query variables
-    variables = {
-        'id': 15125,
     }
 
-    # Prepare headers
-    headers = {
-        'Content-Type': 'application/json',
-    }
+    # Format the data
+    formatted_data = format_anime_data(graphql_data)
 
-    # Prepare the request payload
-    payload = {
-        'query': query,
-        'variables': variables,
-    }
-    
+    # Return the formatted data as JSON
+    return JsonResponse(formatted_data)
 
-    # Make a POST request to the AniList GraphQL API
-    response = requests.post(graphql_url, headers=headers, json=payload)
-
-
-    if response.status_code == 200:
-        # Parse and process the data as needed
-        graphql_data = response.json()
-
-        # Return the processed data as JSON
-        return JsonResponse({'data': graphql_data})
-    else:
-        # Handle error cases
-        return JsonResponse({'error': 'Failed to fetch data from the AniList GraphQL API'}, status=500)
-
-    
+#
+#def anilist_graphql_view(request):
+#
+#    # GraphQL API endpoint URL
+#    graphql_url = 'https://graphql.anilist.co'
+#    
+#    
+#    # Define the GraphQL query
+#    query = '''
+#
+#
+#	query ($id: Int) {
+#      Media (id: $id, type: ANIME) {
+#        id
+#        title {
+#          english          
+#        }
+#        genres
+#    		season
+#      	startDate {
+#      	  year
+#      	  month
+#      	  day
+#      	}
+#      endDate {
+#        year
+#        month
+#        day
+#      }
+#      coverImage {
+#        extraLarge
+#        large
+#        medium
+#        color
+#      }
+#      bannerImage
+#      favourites
+#      description
+#      episodes
+#      trailer {
+#        id
+#      }
+#      
+#      
+#      
+#      }
+#    }
+#    '''
+#        
+#    
+#    # Define query variables
+#    variables = {
+#        'id': 15125,
+#    }
+#
+#    # Prepare headers
+#    headers = {
+#        'Content-Type': 'application/json',
+#    }
+#
+#    # Prepare the request payload
+#    payload = {
+#        'query': query,
+#        'variables': variables,
+#    }
+#    
+#
+#    # Make a POST request to the AniList GraphQL API
+#    response = requests.post(graphql_url, headers=headers, json=payload)
+#
+#    # Extract relevant data
+#    media_data = response['data']['data']['Media']
+#
+#    # Transform genres list into a single string
+#    genres_string = "; ".join(media_data['genres'])
+#
+#    # Create a new dictionary in the desired format
+#    transformed_data = {
+#        "id": media_data['id'],
+#        "title": media_data['title']['english'],
+#        "genre": genres_string,
+#        # Add other fields as needed
+#    }
+#
+#    # Now, transformed_data contains the data in the desired format
+#    return JsonResponse(transformed_data)
+#
+#
+#
+#
+#   # if response.status_code == 200:
+#   #     # Parse and process the data as needed
+#   #     graphql_data = response.json()
+#
+#   #     # Return the processed data as JSON
+#   #     return JsonResponse({'data': graphql_data})
+#   # else:
+#   #     # Handle error cases
+#   #     return JsonResponse({'error': 'Failed to fetch data from the AniList GraphQL API'}, status=500)
+#
+#    
 
 
 
