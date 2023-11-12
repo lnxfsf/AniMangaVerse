a85e49f975b503ced8ddd0a6e70bb7ad8eb82678
Linuxiness <igor.lerinc7@gmail.com>
2023-11-12 13:44:36 +0100

    consuming another API- check

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,7 @@ from .serializers import AnimeSerializer
 
 
 
-# get AniList data
+# get AniList graphQL data
 import requests
 from django.http import JsonResponse
 import json
@@ -16,7 +16,7 @@ import json
 
 
 
-
+# this here, is consuming another API and using it !!!  in it's full glory
 def format_anime_data(graphql_data):
     media_data = graphql_data['data']['Media']
 
@@ -172,107 +172,7 @@ def anilist_graphql_view(request):
     # Return the processed data as JSON
     #return JsonResponse({'data': graphql_data})
 
-#
-#def anilist_graphql_view(request):
-#
-#    # GraphQL API endpoint URL
-#    graphql_url = 'https://graphql.anilist.co'
-#    
-#    
-#    # Define the GraphQL query
-#    query = '''
-#
-#
-#	query ($id: Int) {
-#      Media (id: $id, type: ANIME) {
-#        id
-#        title {
-#          english          
-#        }
-#        genres
-#    		season
-#      	startDate {
-#      	  year
-#      	  month
-#      	  day
-#      	}
-#      endDate {
-#        year
-#        month
-#        day
-#      }
-#      coverImage {
-#        extraLarge
-#        large
-#        medium
-#        color
-#      }
-#      bannerImage
-#      favourites
-#      description
-#      episodes
-#      trailer {
-#        id
-#      }
-#      
-#      
-#      
-#      }
-#    }
-#    '''
-#        
-#    
-#    # Define query variables
-#    variables = {
-#        'id': 15125,
-#    }
-#
-#    # Prepare headers
-#    headers = {
-#        'Content-Type': 'application/json',
-#    }
-#
-#    # Prepare the request payload
-#    payload = {
-#        'query': query,
-#        'variables': variables,
-#    }
-#    
-#
-#    # Make a POST request to the AniList GraphQL API
-#    response = requests.post(graphql_url, headers=headers, json=payload)
-#
-#    # Extract relevant data
-#    media_data = response['data']['data']['Media']
-#
-#    # Transform genres list into a single string
-#    genres_string = "; ".join(media_data['genres'])
-#
-#    # Create a new dictionary in the desired format
-#    transformed_data = {
-#        "id": media_data['id'],
-#        "title": media_data['title']['english'],
-#        "genre": genres_string,
-#        # Add other fields as needed
-#    }
-#
-#    # Now, transformed_data contains the data in the desired format
-#    return JsonResponse(transformed_data)
-#
-#
-#
-#
-#   # if response.status_code == 200:
-#   #     # Parse and process the data as needed
-#   #     graphql_data = response.json()
-#
-#   #     # Return the processed data as JSON
-#   #     return JsonResponse({'data': graphql_data})
-#   # else:
-#   #     # Handle error cases
-#   #     return JsonResponse({'error': 'Failed to fetch data from the AniList GraphQL API'}, status=500)
-#
-#    
+# this here, is consuming another API and using it !!!  in it's full glory
 
 
 
