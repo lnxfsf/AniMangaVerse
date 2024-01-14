bfd23429fed8cb38dcabfb9b5511a19d3d04ab0d
Linuxiness <igor.lerinc7@gmail.com>
2024-01-14 16:31:05 +0100

    login screen design done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -49,97 +49,91 @@ const Login = () => {
 
 
     {/* <!-- form --> */}
-    <div class="container">
-      <div class="forms-container">
-        <div class="signin-signup">
+    <div className="container">
+      <div className="forms-container">
+        <div className="signin-signup">
 
-          <form action="#" class="sign-in-form" onSubmit={loginUser}>
-            <h2 class="title">Sign in</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" name="username" placeholder="Username" required/>
+          <form action="#" className="sign-in-form" onSubmit={loginUser}>
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" name="username" placeholder="Username" required autoComplete="username" />
             </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" name="password" placeholder="Password"  required/>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" name="password" placeholder="Password"  required autoComplete="current-password"/>
             </div>
-            <input type="submit" value="Login" class="btn solid" />
-            <p class="social-text">Or Sign in with social platforms</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
+            <input type="submit" value="Login" className="btn solid" />
+            <p className="social-text">Or Sign in with social platforms</p>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
+              <a href="#" className="social-icon">
+                <i className="fab fa-twitter"></i>
               </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
+              <a href="#" className="social-icon">
+                <i className="fab fa-google"></i>
               </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="#" className="social-icon">
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
           </form>
-          <form action="#" class="sign-up-form">
-            <h2 class="title">Sign up</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Username" required/>
+          <form action="#" className="sign-up-form">
+            <h2 className="title">Create Account</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" name="username1" required  autoComplete="off"/>
             </div>
-            <div class="input-field">
-              <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" required/>
+            <div className="input-field">
+              <i className="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" name="email1" required autocomplete="email"/>
             </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" required/>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" name="password1" required autoComplete="new-password" />
             </div>
-            <input type="submit" class="btn" value="Sign up" />
-            <p class="social-text">Or Sign up with social platforms</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
+            <input type="submit" className="btn" value="Sign up" />
+            <p className="social-text">Or Sign up with social platforms</p>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
+              <a href="#" className="social-icon">
+                <i className="fab fa-twitter"></i>
               </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
+              <a href="#" className="social-icon">
+                <i className="fab fa-google"></i>
               </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="#" className="social-icon">
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
           </form>
         </div>
       </div>
 
-      <div class="panels-container">
-        <div class="panel left-panel">
-          <div class="content">
-            <h3>New here ?</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-              ex ratione. Aliquid!
-            </p>
-            <button onClick={addSignUpModeClass} class="btn transparent" id="sign-up-btn">
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>Welcome Friend !</h3>
+            <p>If you are new and you would like to create an account to unlock more features, sign up.</p>
+            <button onClick={addSignUpModeClass} className="btn transparent" id="sign-up-btn">
               Sign up
             </button>
           </div>
-          <img src="img/log.svg" class="image" alt="" />
+          <img src="login/girl_login.png" className="image" alt="" />
         </div>
-        <div class="panel right-panel">
-          <div class="content">
-            <h3>One of us ?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
-            </p>
-            <button onClick={addSignInModeClass} class="btn transparent" id="sign-in-btn">
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>Welcome Back !</h3>
+            <p>If you already have an account proceed to log in. <br/>Eat your noodles bunny. </p>
+            <button onClick={addSignInModeClass} className="btn transparent" id="sign-in-btn">
               Sign in
             </button>
           </div>
-          <img src="img/register.svg" class="image" alt="" />
+          <img src="login/bunny_register.png" className="image" alt="" />
         </div>
       </div>
     </div>
