aea81e925ac85228b1cd5d3201507d415232683d
Linuxiness <igor.lerinc7@gmail.com>
2024-01-20 16:19:01 +0100

    searchfilter

--- frontend/src/components/Home/BrowseHome/BrowseHome.jsx
+++ frontend/src/components/Home/BrowseHome/BrowseHome.jsx
@@ -3,19 +3,62 @@ import "../../../styles/BrowseHome.scoped.scss";
 import React, { useContext } from "react";
 import AuthContext from "../../../context/AuthContext";
 
+
+import { useState } from 'react';
+
+
 import { Filter } from "./Filter";
 import { Items } from "./Items";
+import { Search } from "../../Search";
 
 const BrowseHome = () => {
   const { user } = useContext(AuthContext);
 
+  
+  // you'll just pass prop from child to parent, in child, just toogle that, and it shows/hides it. without using css..
+  
+  
+  const [showFilter, setShowFilter] = useState(false);
+
+  const handleShowFilterChange = (value) => {
+    setShowFilter(value);
+  };
+
+  console.log(showFilter)
+  
+  
+
   return (
     <>
+
+
+      <div className="bg-blacky flex justify-center items-center ">
+          <Search  onShowFilterChange={handleShowFilterChange}/>
+      </div>
+    
+
+    
+ {showFilter ? (
+
       <div className="main flex justify-center items-center ">
-        <div className="md:basis-1/4 m-4 flex justify-center items-center ">
-          {/* filter  */}
+    
+    
+        <div className="md:basis-1/4 m-4 flex justify-center items-center">
+          {/* filter */}
           <Filter />
         </div>
+      
+    
+
+        {/* <div className="md:basis-1/4 m-4 flex justify-center items-center ">
+    
+
+    
+          <Filter />
+        </div> */}
+
+
+    
 
         <div className="cards_container md:grow md:p-14">
 
@@ -23,6 +66,8 @@ const BrowseHome = () => {
           <Items />
         </div>
       </div>
+   
+     ) : null}   
 
 
       <div className="cards_container_mob grow p-6">
