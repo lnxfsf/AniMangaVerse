ecc345c2f97800ce23d95d50e181d28848b4ed72
Linuxiness <igor.lerinc7@gmail.com>
2024-05-12 13:50:36 +0200

    improved fetching for dropdown
    to fetch directly from mongodb

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,44 +1,78 @@
 import axios from "axios";
 
-
-
-
-
-
 const apiInstance = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "http://localhost:5000/api",
 });
 
 const loaderInterval = setInterval(() => {
   console.log("Loading...");
 }, 1000);
 
+let studio = [];
+let genres = [];
+let season = [];
+let episodes = [];
+
 export let anime_data = [];
 
+apiInstance
+  .get("/ListAnime")
+  .then((response) => {
+    anime_data = response.data;
+    console.log(response.data);
 
+    // uzima samo dostupne studios iz animes koje ima
+    studio = anime_data.map((anime) => anime.studio);
 
-  apiInstance
-    .get("/ListAnime")
-    .then((response) => {
+    // uzima samo dostupne genres iz animes koje ima
+    anime_data.forEach((anime) => {
+      anime.genre.forEach((genre) => {
+        let formattedGenre = genre.charAt(0).toUpperCase() + genre.slice(1);
+        if (!genres.includes(formattedGenre)) {
+          genres.push(formattedGenre);
+        }
+      });
 
-      anime_data = response.data;
-      console.log(response.data);
-      return anime_data;
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    })
-    .finally(() => {
-      clearInterval(loaderInterval);
-      console.log("Loading finished.");
+      // ovo sada za sezone, prvo najveci, i onda ide od najmanji do najveceg...
+      let highestSeason = 0;
+      anime_data.forEach((anime) => {
+        if (anime.seasons > highestSeason) {
+          highestSeason = anime.seasons;
+        }
+      });
+
+      for (let i = 1; i <= highestSeason; i++) {
+        if (!season.includes(i)) {
+          season.push(i);
+        }
+      }
+
+      // to isto i za epizode
+
+      let highestEpisode = 0;
+      anime_data.forEach((anime) => {
+        if (anime.episodes > highestEpisode) {
+          highestEpisode = anime.episodes;
+        }
+      });
+
+      for (let i = 1; i <= highestEpisode; i++) {
+        if (!episodes.includes(i)) {
+          episodes.push(i);
+        }
+      }
     });
 
+    return anime_data;
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  })
+  .finally(() => {
+    clearInterval(loaderInterval);
+    console.log("Loading finished.");
+  });
 
-
-
-
-
-    
 // dummy manga data values
 export const manga_data = [
   {
@@ -351,149 +385,14 @@ export const manga_data = [
   },
 ];
 
-
-
-// options for dropdowns
-export const genres = [
-  "Action",
-  "Adventure",
-  "Cars",
-  "Comedy",
-  "Dementia",
-  "Demons",
-  "Drama",
-  "Ecchi",
-  "Fantasy",
-  "Game",
-  "Historical",
-  "Horror",
-  "Josei",
-  "Kids",
-  "Magic",
-  "Martial Arts",
-  "Mecha",
-  "Military",
-  "Music",
-  "Mystery",
-  "Parody",
-  "Police",
-  "Psychological",
-  "Romance",
-  "Samurai",
-  "School",
-  "Sci-Fi",
-  "Seinen",
-  "Shoujo",
-  "Shoujo Ai",
-  "Shounen",
-  "Shounen Ai",
-  "Slice of Life",
-  "Space",
-  "Sports",
-  "Super Power",
-  "Supernatural",
-  "Thriller",
-  "Vampire",
-];
-
 export const year = [
-  2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011,
-  2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998,
-  1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990, 1989, 1988, 1987, 1986, 1985,
-  1984, 1983, 1982, 1981, 1980, 1979, 1978, 1977, 1976, 1975, 1974, 1973, 1972,
-  1971, 1970, 1969, 1968, 1967, 1966, 1965, 1964, 1963, 1962, 1961, 1960, 1959,
-  1958, 1957, 1956, 1955, 1954, 1953, 1952, 1951, 1950, 1949, 1948, 1947, 1946,
-  1945, 1944, 1943, 1942, 1941, 1940,
-];
-
-// TODO malo advancement query, da uzme iz 'studio' svih polja i doda u listu. NA BACkEND to uradi. i samo da vraća listu, na neki route... da koristis samo za koje treba
-export const studio = [
-  "3Hz",
-  "A.C.G.T",
-  "A.P.P.P.",
-  "Actas",
-  "Ajia-do Animation Works",
-  "Anime International Company (AIC)",
-  "Arms Corporation (defunct)",
-  "Artland",
-  "Artmic (defunct)",
-  "Arvo Animation",
-  "Nippon Rois Cartoon (Regional Service Division for Japan from Indonesia)",
-  "Ashi Productions",
-  "Asahi Production",
-  "Asread",
-  "AXsiZ",
-  "Bandai Namco Pictures",
-  "Bee Train",
-  "Bibury Animation Studios",
-  "Blue Lynx",
-  "Bones",
-  "Brain's Base",
-  "Bridge (studio)",
-  "C2C",
-  "Chaos Project",
-  "CoMix Wave Films",
-  "Connect (studio) (defunct, absorbed by Silver Link and then dissolved)",
-  "Creators in Pack",
-  "C-Station",
-  "CygamesPictures",
-  "Daume",
-  "David Production",
-  "Diomeda",
-  "DLE",
-  "Doga Kobo",
-  "ENGI",
-  "EMT Squared",
-];
-
-export const season = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-  80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
-  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
-  115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129,
-  130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144,
-  145, 146, 147, 148, 149,
-];
-
-export const episodes = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-  80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
-  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
-  115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129,
-  130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144,
-  145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
-  160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174,
-  175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189,
-  190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204,
-  205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219,
-  220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234,
-  235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249,
-  250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264,
-  265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279,
-  280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294,
-  295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309,
-  310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324,
-  325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339,
-  340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354,
-  355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369,
-  370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384,
-  385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399,
-  400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414,
-  415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429,
-  430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444,
-  445, 446, 447, 448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459,
-  460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474,
-  475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489,
-  490, 491, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504,
-  505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519,
-  520, 521, 522, 523, 524, 525, 526, 527, 528, 529, 530, 531, 532, 533, 534,
-  535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 549,
+  2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012,
+  2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999,
+  1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990, 1989, 1988, 1987, 1986,
+  1985, 1984, 1983, 1982, 1981, 1980, 1979, 1978, 1977, 1976, 1975, 1974, 1973,
+  1972, 1971, 1970, 1969, 1968, 1967, 1966, 1965, 1964, 1963, 1962, 1961, 1960,
+  1959, 1958, 1957, 1956, 1955, 1954, 1953, 1952, 1951, 1950, 1949, 1948, 1947,
+  1946, 1945, 1944, 1943, 1942, 1941, 1940,
 ];
 
 export const publishing = [
@@ -504,4 +403,8 @@ export const publishing = [
   "Cancelled",
 ];
 
+//TODO, i search, za epizode i ostalo, to treba samo, sve što je manje od toga..
+
 export const country_origin = ["Japan", "South Corea", "China", "Taiwan"];
+
+export { studio, genres, season, episodes };
